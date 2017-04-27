117d6425e2f6a34567f78aa7428d6cfa59ef5c54
moises <moises@abelltech.com>
2017-04-28 00:34:05 +0800

    Initial WP Files

---
+++ facebook/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<meta property="fb:app_id" content="231000980349688" />
+</head>
+
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=231000980349688";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<table>
+<tbody>
+<tr><td><div class="fb-comments" data-href="http://vimmi.net/facebook/index.php" data-numposts="5"></div></td></tr>
+</tbody>
+</table>
+</body>
+</html>
